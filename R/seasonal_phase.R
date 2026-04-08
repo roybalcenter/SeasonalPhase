0bebb4f21428724fb41dbca279b7fecd20a9efde
David Conroy <54281685+conroylab@users.noreply.github.com>
2026-04-08 18:28:46 -0400

    Update seasonal_phase.R

--- R/seasonal_phase.R
+++ R/seasonal_phase.R
@@ -1,7 +1,6 @@
 # Sample R script for transforming a date into an astronomical seasonal phase variable
-# - Code produced by Gemini on April 7, 2026 
-# - Oriented around northern hemisphere
-# - For single year date ranges that are not in a leap year, change the number of days to 365
+# - Code produced by Gemini on April 8, 2026 
+# - Oriented around summer solstice in the northern hemisphere; floating point estimation used the equinoxes and winter solstice may not have exact values of 0 and -1 
 # - Ensure your column is of class Date or POSIXct. If you import from a CSV, you may need to wrap your column in as.Date(df$date) before passing it to the function.
 #    ensure the input column is a true datetime object using pd.to_datetime() before running the function, otherwise .dt.dayofyear will throw an error.
 # - Check your wave by running plot(df$date, df$seasonal_phase, type="l") to ensure the peaks and valleys land exactly where you expect them.
